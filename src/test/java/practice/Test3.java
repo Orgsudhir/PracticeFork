1b97a6a231a88f8bfedf0456a44d90f46da376d4
camsu <camsu@host.docker.internal>
2023-11-11 10:50:23 +0530

    add

--- src/test/java/practice/Test3.java
+++ src/test/java/practice/Test3.java
@@ -3,7 +3,7 @@ package practice;
 public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("post import Project3");
+		System.out.println("pre requisite import Project3");
 
 	}
 
